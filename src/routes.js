a0d36e3ec768301c5b6933b514b3ee37b0900770
yn9988 <e0324469@u.nus.edu>
2021-04-02 13:25:39 +0800

    modified routes.js

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ import ContactUs from './components/ContactUs'
 import FAQ from './components/FAQ'
 import HomeTutor from './components/HomeTutor'
 import HomeStudent from './components/HomeStudent'
+import TutorDetails from './components/TutorDetails'
 
 
 export default [
@@ -14,5 +15,6 @@ export default [
     { path: '/contactus', component: ContactUs},
     { path: '/faq', component: FAQ},
     { path: '/homeTutor', component: HomeTutor},
-    { path: '/homeStudent', component: HomeStudent}
+    { path: '/homeStudent', component: HomeStudent},
+    {path: '/tutorDetails', component: TutorDetails}
 ]
